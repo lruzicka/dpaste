eb700d4e65ece603bbdc575a452dcc7d736cd90c
Lukas Ruzicka <lruzicka@redhat.com>
2022-03-03 16:25:57 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dpaste-console",
-    version="1.1.2",
+    version="1.1.3",
     py_modules=['dpaste'],
     author="Lukáš Růžička",
     author_email="lruzicka@redhat.com",
